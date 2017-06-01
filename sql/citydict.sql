6793b243e31de47bc1ab9364e56b8152bb0bf4e2
zhangleilei <always_0122@163.com>
2017-06-01 18:45:08 +0800

    add sql

---
+++ sql/citydict.sql
@@ -0,0 +1,40 @@
+/*
+-- Query: SELECT * FROM mx_online.organization_citydict
+LIMIT 0, 1000
+
+-- Date: 2017-06-01 18:43
+*/
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (2,'北京市','北京市',110000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (3,'天津市','天津市',120000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (4,'河北省','河北省',130000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (5,'山西省','山西省',140000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (6,'内蒙古自治区','内蒙古自治区',150000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (7,'辽宁省','辽宁省',210000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (8,'吉林省','吉林省',220000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (9,'黑龙江省','黑龙江省',230000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (10,'上海市','上海市',310000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (11,'江苏省','江苏省',320000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (12,'浙江省','浙江省',330000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (13,'安徽省','安徽省',340000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (14,'福建省','福建省',350000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (15,'江西省','江西省',360000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (16,'山东省','山东省',370000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (17,'河南省','河南省',410000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (18,'湖北省','湖北省',420000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (19,'湖南省','湖南省',430000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (20,'广东省','广东省',440000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (21,'广西壮族自治区','广西壮族自治区',450000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (22,'海南省','海南省',460000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (23,'重庆市','重庆市',500000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (24,'四川省','四川省',510000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (25,'贵州省','贵州省',520000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (26,'云南省','云南省',530000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (27,'西藏自治区','西藏自治区',540000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (28,'陕西省','陕西省',610000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (29,'甘肃省','甘肃省',620000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (30,'青海省','青海省',630000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (31,'宁夏回族自治区','宁夏回族自治区',640000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (32,'新疆维吾尔自治区','新疆维吾尔自治区',650000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (33,'台湾省','台湾省',710000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (34,'香港特别行政区','香港特别行政区',810000,'2017-05-18 14:48:03.000000');
+INSERT INTO `organization_citydict` (`id`,`name`,`desc`,`code`,`create_time`) VALUES (35,'澳门特别行政区','澳门特别行政区',820000,'2017-05-18 14:48:03.000000');
